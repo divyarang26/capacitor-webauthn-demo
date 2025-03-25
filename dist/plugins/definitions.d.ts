0a82a10089acc899c2859d515b6d798add87daac
divyarang26 <divya.rang@imentus.com>
2025-03-25 23:53:15 +0530

    update

---
+++ dist/plugins/definitions.d.ts
@@ -0,0 +1,9 @@
+import { PublicKeyCredentialCreationOptionsJSON, PublicKeyCredentialRequestOptionsJSON, RegistrationResponseJSON, AuthenticationResponseJSON } from "./type";
+export type { PublicKeyCredentialCreationOptionsJSON, PublicKeyCredentialRequestOptionsJSON, RegistrationResponseJSON, AuthenticationResponseJSON, };
+export interface WebAuthnPlugin {
+    isWebAuthnAvailable(): Promise<{
+        value: boolean;
+    }>;
+    create(publicKeyCredentialCreationOptionsJSON: PublicKeyCredentialCreationOptionsJSON): Promise<RegistrationResponseJSON>;
+    get(requestOptionsJSON: PublicKeyCredentialRequestOptionsJSON, useBrowserAutofill?: boolean): Promise<AuthenticationResponseJSON>;
+}
